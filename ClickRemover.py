2dcabac8fbba45efac9c24348c65f743cbe6ee02
Piotr Siewiera <piotr.siewiera@student.tugraz.at>
2026-04-10 17:39:09 +0200

    Initial commit

---
+++ ClickRemover.py
@@ -0,0 +1,44 @@
+import ast
+
+
+class ClickRemover(ast.NodeTransformer):
+    def visit_FunctionDef(self, node):
+        i = 0
+        while i < len(node.body):
+            if (i + 1 < len(node.body) and
+                    isinstance(node.body[i], ast.Expr) and
+                    isinstance(node.body[i + 1], ast.Expr) and
+                    (node.body[i].value.func.attr == 'click' or node.body[i].value.func.attr == 'press') and
+                    node.body[i + 1].value.func.attr == 'fill'):
+                try:
+                    current = node.body[i].value
+                    next_node = node.body[i + 1].value
+
+                    click_locator = current.func.value.args[0].value
+                    fill_locator = next_node.func.value.args[0].value
+
+                    if click_locator == fill_locator:
+                        del node.body[i]
+                        continue
+                except (AttributeError, IndexError):
+                    pass
+            elif (i > 0 and
+                    isinstance(node.body[i], ast.Expr) and
+                    isinstance(node.body[i - 1], ast.Expr) and
+                    node.body[i].value.func.attr == 'fill' and
+                    node.body[i+1].value.func.attr == 'press'):
+                try:
+                    print(i)
+                    current = node.body[i].value
+                    next_node = node.body[i+1].value
+
+                    click_locator = current.func.value.args[0].value
+                    fill_locator = next_node.func.value.args[0].value
+
+                    if click_locator == fill_locator:
+                        del node.body[i+1]
+                        continue
+                except (AttributeError, IndexError):
+                    pass
+            i += 1
+        return node
---
+++ test.py
@@ -0,0 +1,51 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+class LoginPage:
+    def __init__(self, page):
+        self.page = page
+
+    def login(self, login, password):
+        self.page.locator("[data-test=\"username\"]").fill(login)
+        self.page.locator("[data-test=\"password\"]").fill(password)
+        self.page.locator("[data-test=\"login-button\"]").click()
+
+    def expect_text(self, text):
+        expect(self.page.locator("[data-test=\"error\"]")).to_contain_text(
+            text)
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://www.saucedemo.com/")
+    page.locator("[data-test=\"username\"]").click()
+    page.locator("[data-test=\"username\"]").fill("standard_user")
+    page.locator("[data-test=\"password\"]").click()
+    page.locator("[data-test=\"password\"]").fill("secret_sauce")
+    page.locator("[data-test=\"login-button\"]").click()
+    expect(page.locator("[data-test=\"item-4-title-link\"]")).to_be_visible()
+    expect(page.locator("[data-test=\"item-0-title-link\"] [data-test=\"inventory-item-name\"]")).to_contain_text("Sauce Labs Bike Light")
+    expect(page.locator("[data-test=\"product-sort-container\"]")).to_have_value("az");
+    page.get_by_text("$29.99").click()
+    page.get_by_text("Name (A to Z)Name (A to Z)").click()
+    page.locator("[data-test=\"product-sort-container\"]").select_option("lohi")
+    page.locator("div").filter(has_text="Swag Labs").nth(5).click()
+    page.goto("https://www.saucedemo.com/inventory.html")
+    page.get_by_role("button", name="Open Menu").click()
+    page.locator("[data-test=\"logout-sidebar-link\"]").click()
+    page.locator("[data-test=\"username\"]").click()
+    page.locator("[data-test=\"username\"]").fill("locked_out_user")
+    page.locator("[data-test=\"username\"]").press("Tab")
+    page.locator("[data-test=\"password\"]").fill("secret_sauce")
+    page.locator("[data-test=\"login-button\"]").click()
+    expect(page.locator("[data-test=\"error\"]")).to_contain_text("Epic sadface: Sorry, this user has been locked out.")
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
