31910778aba873c5a06eb801441d190ac7f8f342
Piotr Siewiera <piotr.siewiera@student.tugraz.at>
2026-05-25 16:53:48 +0200

    ClickRemover.py bug fix

--- ClickRemover.py
+++ ClickRemover.py
@@ -1,47 +1,43 @@
 import ast
 
+from Normalize import normalize_instruction
 
 class ClickRemover(ast.NodeTransformer):
     def visit_FunctionDef(self, node):
         i = 0
         while i < len(node.body):
-            if (i + 1 < len(node.body) and
-                    isinstance(node.body[i], ast.Expr) and
-                    isinstance(node.body[i + 1], ast.Expr) and
-                    isinstance(node.body[i].value, ast.Call) and
-                    isinstance(node.body[i + 1].value, ast.Call) and
-                    isinstance(node.body[i].value.func, ast.Attribute) and
-                    isinstance(node.body[i+1].value.func, ast.Attribute) and
-                    (node.body[i].value.func.attr == 'click' or node.body[i].value.func.attr == 'press') and
-                    node.body[i + 1].value.func.attr == 'fill'):
-                try:
-                    current = node.body[i].value
-                    next_node = node.body[i + 1].value
+            if i + 1 < len(node.body):
+                normalized = normalize_instruction(node.body[i])
+                next_normalized = normalize_instruction(node.body[i + 1])
 
-                    click_locator = current.func.value.args[0].value
-                    fill_locator = next_node.func.value.args[0].value
+                if normalized is not None and next_normalized is not None:
+                    if ((normalized.action == 'click' or normalized.action == 'press')
+                            and next_normalized.action == 'fill'):
+                        if normalized.locator_arguments is not None and len(normalized.locator_arguments) > 0:
+                            if next_normalized.locator_arguments is not None and len(next_normalized.locator_arguments) > 0:
+                                for n_argument, next_n_argument in zip(normalized.locator_arguments, next_normalized.locator_arguments):
+                                    if n_argument == next_n_argument:
+                                        del node.body[i]
+                                        continue
 
-                    if click_locator == fill_locator:
-                        del node.body[i]
-                        continue
-                except (AttributeError, IndexError):
-                    pass
-            elif (i > 0 and
-                    isinstance(node.body[i], ast.Expr) and
-                    isinstance(node.body[i - 1], ast.Expr) and
-                    node.body[i].value.func.attr == 'fill' and
-                    node.body[i+1].value.func.attr == 'press'):
-                try:
-                    current = node.body[i].value
-                    next_node = node.body[i+1].value
+                            elif next_normalized.locator_keywords is not None and len(next_normalized.locator_keywords) > 0:
+                                for n_argument, next_n_keyword in zip(normalized.locator_arguments, next_normalized.locator_keywords):
+                                    if n_argument == next_normalized.locator_keywords[next_n_keyword]:
+                                        del node.body[i]
+                                        continue
 
-                    click_locator = current.func.value.args[0].value
-                    fill_locator = next_node.func.value.args[0].value
+                        if normalized.locator_keywords is not None and len(normalized.locator_keywords) > 0:
+                            if next_normalized.locator_arguments is not None and len(next_normalized.locator_arguments) > 0:
+                                for n_keyword, next_n_argument in zip(normalized.locator_keywords, next_normalized.locator_arguments):
+                                    if normalized.locator_keywords[n_keyword] == next_n_argument:
+                                        del node.body[i]
+                                        continue
 
-                    if click_locator == fill_locator:
-                        del node.body[i+1]
-                        continue
-                except (AttributeError, IndexError):
-                    pass
+                            elif next_normalized.locator_keywords is not None and len(next_normalized.locator_keywords) > 0:
+                                for key, n_value in normalized.locator_keywords.items():
+                                    if n_value == next_normalized.locator_keywords[key]:
+                                        del node.body[i]
+                                        continue
+
             i += 1
         return node
